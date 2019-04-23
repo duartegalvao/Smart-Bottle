4589b7ca5e2484427da5b56043b1461f1fec39da
Pethrus Gärdborn <gardborn@kth.se>
2019-04-23 11:03:43 +0200

    Dynamic readings of bottle updates. TODO: Make simple client. Send and read timestamps. Start creating basic 'health analysis' for user.

--- bottle/bottleAnalytics/urls.py
+++ bottle/bottleAnalytics/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
         path('', views.index, name='index'),
         path('bottleUpdate', views.bottleUpdate, name='bottleUpdate'),
+#        path('bottleUpdate/<int:temp>/<int:weight>', views.bottleUpdate, name='bottleUpdate'),
         path('getHealthStatus', views.getHealthStatus, name='getHealthStatus')
             ]
--- bottle/bottleAnalytics/views.py
+++ bottle/bottleAnalytics/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from bottleAnalytics.models import BottleReading
+from django.views.decorators.csrf import csrf_exempt
+
+# TEST bottleUpdate
+# curl --data "temp=10&weight=11" localhost:8000/bottleAnalytics/bottleUpdate
 
 def index(request):
     return HttpResponse("This is the bottle index.")
 
-# Comes from the bottle
+# Receives parameters from the bottle
+@csrf_exempt
 def bottleUpdate(request):
-    # TODO Update BottleStats with the readings received in the request
     newBottleReading = BottleReading()
-    newBottleReading.temp = 77
-    newBottleReading.weight = 78
+    newBottleReading.temp = request.POST.get('temp')
+    newBottleReading.weight = request.POST.get('weight')
     newBottleReading.save()
     return HttpResponse("BottleStats has been updated")
 
-# Comes from the user
+# Shows the current "health status" to the user of the bottle
 def getHealthStatus(request):
     # TODO Run the "health algorithm" and respond with current health status.
     return HttpResponse("Your current health status is: ")
