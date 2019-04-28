121d580363b0500bbd5b38bd0bb092ec3bd75b6e
Duarte Galvão <duarte.galvao@rnl.tecnico.ulisboa.pt>
2019-04-28 15:26:39 +0200

    Simplify find stable points and implement get consumption

--- bottle/bottleAnalytics/classify.py
+++ bottle/bottleAnalytics/classify.py
@@ -4,34 +4,36 @@ import numpy as np
 def find_stable_points(w_raw, var_threshold=0.1, min_stable_samples=3):
     """Return list of stable weight points."""
     run_w = []
-    run_t = []
     w_stable = []
 
-    for v in w_raw:
-        if len(run_w) == 0 or np.var(run_w + [v.weight]) <= var_threshold:
-            run_w.append(v.weight)
-            run_t.append(v.time)
+    for w in w_raw:
+        if len(run_w) == 0 or np.var(run_w + [w]) <= var_threshold:
+            run_w.append(w)
         else:
             if len(run_w) >= min_stable_samples:
-                w_stable.append((np.mean(run_t), np.mean(run_w)))
-            run_w = [v.weight, ]
-            run_t = [v.time, ]
+                w_stable.append(np.mean(run_w))
+            run_w = [w, ]
 
     # Insert last
     if len(run_w) >= min_stable_samples:
-        w_stable.append((np.mean(run_t), np.mean(run_w)))
+        w_stable.append(np.mean(run_w))
 
     return w_stable
 
 
-def get_consumptions(W_stable):
-    consumptions = []
-    # TODO
+def get_consumption(w_stable):
+    consumption = 0
+    prev_w = None
 
-    return consumptions
+    for w in w_stable:
+        if prev_w is not None and w < prev_w:
+            consumption += prev_w - w
+        prev_w = w
+
+    return consumption
 
 
-def calculate_score(consumptions, temperatures):
+def calculate_score(consumption, temperatures):
     score = 0
     # TODO
 
@@ -40,7 +42,7 @@ def calculate_score(consumptions, temperatures):
 
 def classify(data):
     w_stable = find_stable_points(data)
-    consumptions = get_consumptions(w_stable)
-    score = calculate_score(consumptions, data)
+    consumption = get_consumption(w_stable)
+    score = calculate_score(consumption, data)
 
     return score
