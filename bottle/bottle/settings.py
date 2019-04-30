bbd93f3bdac44aea90d4197f3797542b894eacea
Duarte Galvão <duarte.galvao@rnl.tecnico.ulisboa.pt>
2019-04-30 14:19:06 +0200

    Add django-solo to installed apps

--- bottle/bottle/settings.py
+++ bottle/bottle/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'bottleAnalytics.apps.BottleanalyticsConfig',
+    'solo.apps.SoloAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
