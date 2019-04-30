d88b585ec0629a4fb892e53c61cbc261cbd0b2de
Duarte Galvão <duarte.galvao@rnl.tecnico.ulisboa.pt>
2019-04-30 14:14:11 +0200

    Add user settings singleton

--- bottle/bottleAnalytics/models.py
+++ bottle/bottleAnalytics/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from datetime import datetime
+from solo.models import SingletonModel
 
 
 class BottleReading(models.Model):
     temp = models.IntegerField(default=150)
     weight = models.IntegerField(default=150)
     time = models.DateTimeField(default=datetime.now)
+
+
+class UserSettings(SingletonModel):
+    birth_date = models.DateField(default=None, blank=True, null=True)
+    weight = models.FloatField(default=65)
